f28db04141e2913a5ba2d289059e550bb39fd824
Said Khail <saidkhail@hotmail.com>
2025-04-29 14:23:15 +0100

    Added: Login Tests, A basic github action workflow

--- mysite/core/tests.py
+++ mysite/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class LoginPageTests(TestCase):
+    
+    def test_login_page_status(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_login_template_used(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'login.html')
