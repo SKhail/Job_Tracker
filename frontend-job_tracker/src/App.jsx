0840d0ba8527c30bde0e44bbe11f33a8f2267ba3
Said Khail <saidkhail@hotmail.com>
2025-05-11 20:47:24 +0100

    Frontend: Kanban Board rough design has been implemented with static data

--- frontend-job_tracker/src/App.jsx
+++ frontend-job_tracker/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import JobList from "./components/JobList";
+import KanbanBoard from "./components/KanbanBoard";
 
 function App() {
   return (
     <>
       <div className="p-4">
         <h1 className="font-bold text-xl">Job Tracker </h1>
-        <JobList />
+        <KanbanBoard />
       </div>
     </>
   );
